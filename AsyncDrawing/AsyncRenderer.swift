ae77c39220fbd16882d55318fb6dd9fd0fbfe2aa
Ahmed Khalaf <akhalaf@arabia-it.com>
2019-07-02 15:01:33 +0200

    faster version

---
+++ AsyncDrawing/AsyncRenderer.swift
@@ -0,0 +1,44 @@
+//
+//  AsyncRenderer.swift
+//  AsyncDrawing
+//
+//  Created by Arabia -IT on 7/2/19.
+//  Copyright © 2019 Arabia-IT. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+import UIKit
+
+class AsyncRenderer {
+    private let queue: DispatchQueue
+    private let text: NSAttributedString
+    
+    init(queue: DispatchQueue = DispatchQueue(label: "AsyncRenderer"), text: NSAttributedString) {
+        self.queue = queue
+        self.text = text
+    }
+    
+    typealias Completion = (CGSize, CGImage?) -> Void
+    func draw(size: CGSize, completion: @escaping Completion) {
+        queue.async {
+            let size = self.measure(constrainedSize: size)
+            completion(size, self.render(size: size))
+        }
+    }
+    
+    private func measure(constrainedSize: CGSize) -> CGSize {
+        return text.boundingRect(with: constrainedSize, options: [.usesFontLeading, .usesLineFragmentOrigin], context: nil).size
+    }
+    
+    private func render(size: CGSize) -> CGImage? {
+        UIGraphicsBeginImageContextWithOptions(size, false, 0)
+        defer {
+            UIGraphicsEndImageContext()
+        }
+        
+        text.draw(in: CGRect(origin: .zero, size: size))
+        
+        return UIGraphicsGetImageFromCurrentImageContext()?.cgImage
+    }
+}
---
+++ AsyncDrawing/ItemVC.swift
@@ -0,0 +1,57 @@
+//
+//  ItemVC.swift
+//  AsyncDrawing
+//
+//  Created by Arabia -IT on 7/2/19.
+//  Copyright © 2019 Arabia-IT. All rights reserved.
+//
+
+import UIKit
+
+class ItemVC: UIViewController {
+    private let renderer = AsyncRenderer(text: NSAttributedString(string: """
+دار سقطت غرّة، مع, ألمّ التحالف وقد تم. لمحاكم بمحاولة أي بال, ان انه تحرير الأرضية, للصين قائمة الثقيلة ما حين. عل كلّ مقاطعة العسكري, تم بحق ثانية بزمام وفرنسا. خلاف لكون الجنود بعض ان, أوزار الأولى للمجهود شيء بل. انه قد السيء الجنوب, شواطيء الثالث من بها.
+
+و حدى أفاق فاتّبع. فعل أوزار نتيجة ليتسنّى ما, بين بل الصينية الجنرال. في مدن الباهضة المعاهدات, أما يتبقّ اوروبا في, بحث عرفها اتّجة البرية عن. فسقط فرنسا الأسيوي إذ بلا, جعل ومضى أملاً علاقة إذ.
+
+مع لكل واحدة الستار, الجوي القوى بالجانب بين أم. تم والحزب سبتمبر جعل, يبق وقرى تصرّف مليون هو. المضي الصفحة المنتصر عل دول. لم خلاف يتسنّى الباهضة بلا.
+
+جيما وحرمان أخر أي, مشروط وبالتحديد، لم هذه, أي قبل مارد الخطّة اتفاقية. بحق إذ يقوم المبرمة. بخطوط يتسنّى تم ضرب, فقد ما وجزر بقيادة. أخر مع مرمى وحتى الأوروبية.
+
+تصفح فهرست بالسيطرة بال ما. بحق تم ويعزى وانهاء, لكل من الضروري الإقتصادي. فعل و جديدة بلاده غينيا. ان لها واُسدل المشترك الخارجية, قد فصل واستمر واشتدّت ويكيبيديا. أطراف الشمال شموليةً بـ لكل, أم مارد والفلبين غير.
+
+""", attributes: [
+    NSAttributedString.Key.foregroundColor : UIColor.black,
+    NSAttributedString.Key.font: UIFont.systemFont(ofSize: 20),
+    NSAttributedString.Key.paragraphStyle: { let mps = NSMutableParagraphStyle(); mps.alignment = .right; return mps }()
+    ]))
+    
+    @IBOutlet private weak var textView: UIView!
+    @IBOutlet private weak var textViewHeightConstraint: NSLayoutConstraint!
+    
+    private var viewWillAppearQueue = [() -> ()]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        viewWillAppearQueue.append {
+            self.renderer.draw(size: CGSize(width: self.textView.bounds.width, height: .infinity)) { (size, cgImage) in
+                DispatchQueue.main.async {
+                    self.textViewHeightConstraint.constant = size.height
+                    
+                    self.textView.layer.contentsScale = UIScreen.main.scale
+                    self.textView.layer.contentsGravity = .right
+                    self.textView.layer.contents = cgImage
+                }
+            }
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        while !viewWillAppearQueue.isEmpty {
+            viewWillAppearQueue.removeFirst()()
+        }
+    }
+}
